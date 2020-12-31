1c6213e77baeda160192d220da1eff006371093c
HayatoMiyamura <make.better.12345@gmail.com>
2020-12-31 16:05:10 +0900

    gitignoreを反映

--- .nuxt/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Home } from '../../src/components/Home.vue'
-export { default as Logo } from '../../src/components/Logo.vue'
-export { default as Mypage } from '../../src/components/Mypage.vue'
-export { default as VuetifyLogo } from '../../src/components/VuetifyLogo.vue'
-
-export const LazyHome = import('../../src/components/Home.vue' /* webpackChunkName: "components/Home" */).then(c => c.default || c)
-export const LazyLogo = import('../../src/components/Logo.vue' /* webpackChunkName: "components/Logo" */).then(c => c.default || c)
-export const LazyMypage = import('../../src/components/Mypage.vue' /* webpackChunkName: "components/Mypage" */).then(c => c.default || c)
-export const LazyVuetifyLogo = import('../../src/components/VuetifyLogo.vue' /* webpackChunkName: "components/VuetifyLogo" */).then(c => c.default || c)
--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['checkauthenticated'] = require('../src/middleware/checkauthenticated.ts')
-middleware['checkauthenticated'] = middleware['checkauthenticated'].default || middleware['checkauthenticated']
-
-export default middleware
